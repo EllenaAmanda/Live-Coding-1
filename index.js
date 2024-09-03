54df758cc31ddf936aed131f873d483cffa4fa2e
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-03 20:30:02 +0700

    commit 3

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let arrNum = [8 ,3 ,9 ,2 ,5]
 let input = 3;
 let angkaDicari;
-for (let i=0; i<arrNum.length; i++){
-    if(arrNum[0]==input){
-        return angkaDicari=input
+
+for (let i=0; i < arrNum.length; i++){
+    if(arrNum[0]===input){
+        input
     }
 }
