5d3a9d7ef4b51bd9e29ec49eb28c042fb23ed937
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-03 20:41:06 +0700

    commit 5

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
-let arrNum = [8 ,3 ,9 ,2 ,5]
-let input = 3;
-let angkaDicari;
+// let array = [8 ,3 ,9 ,2 ,5]
+// let input = 3;
 
-for (let i=0; i < arrNum.length; i++){
-    if(arrNum[0] === input){
-        console.log(input)        
+function cariAngka(arrNum, input){
+    for (let i=0; i < arrNum.length; i++){
+        
+        if(arrNum[i] === input){
+            // console.log(input)    
+            return input
+        }
     }
 }
+
+console.log(cariAngka([8 ,3 ,9 ,2 ,5], 3))
