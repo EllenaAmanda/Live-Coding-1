ce5057799c8971040b3c30aed84dc4379587cf1b
EllenaAmanda <ellenaamanda846@gmail.com>
2024-09-03 20:49:40 +0700

    commit 6

--- index.js
+++ index.js
@@ -2,13 +2,20 @@
 // let input = 3;
 
 function cariAngka(arrNum, input){
+    if(typeof input !== 'number' ){
+        return "inputan harus number"
+    }
+
     for (let i=0; i < arrNum.length; i++){
-        
         if(arrNum[i] === input){
             // console.log(input)    
             return input
         }
     }
+    
+    return false
 }
 
 console.log(cariAngka([8 ,3 ,9 ,2 ,5], 3))
+console.log(cariAngka([8 ,3 ,9 ,2 ,5], 1))
+console.log(cariAngka([8 ,3 ,9 ,2 ,5], "h"))
